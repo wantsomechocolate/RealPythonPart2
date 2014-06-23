3bade7b518194420471fc6efa87897a081f13f57
wantsomechocolate <wantsomechocolate@gmail.com>
2014-06-23 19:42:43 -0400

    Done with chapter5

--- projects/hello-world-flask/app.py
+++ projects/hello-world-flask/app.py
@@ -6,13 +6,22 @@ from flask import Flask
 # create the application object
 app = Flask(__name__)
 
+# err handling
+app.config["DEBUG"]=True
+
 # use decorators to link the function to a url
 @app.route("/")
 @app.route("/hello")
 
+
 # define the view using a function, which returns a string
 def hello_world():
 	return "Hello, World!"
+
+#dynamic route
+@app.route("/test/<search_query>")
+def search(search_query):
+	return search_query
 
 # start the development server using the run() method
 if __name__ == "__main__":
